be2d4fd46c69e02975d2faa8e60bd85759b780a3
Noah <rosenzweig.noah@gmail.com>
2018-04-05 00:43:58 +0200

    Fix gitignore, other minor fixes

--- src/rb/Game.rb
+++ src/rb/Game.rb
@@ -16,7 +16,7 @@ PLAYER.goto! Instances::Rooms::ParsleysTruck.new
 ## Savefile
 # Load development Savefile
 SAVEFILE = Saves::Savefile.new 'development'
-SAVEFILE.restore
+SAVEFILE.restore  unless (ENVT.test?)
 
 ### Game
 class Game
